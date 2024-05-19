2c43d04fa156b3b7e6afeed4614c033ba573cd02
blakerandle <blakerandlecs@gmail.com>
2024-05-19 15:29:02 -0700

    Update SurveyManager.sol

--- survey_money/src/SurveyManager.sol
+++ survey_money/src/SurveyManager.sol
@@ -8,10 +8,11 @@ contract SurveyManager {
         bool active;
         string question;
         string[] answers;
+        uint256[] answerCounts;
         mapping(address => bool) hasUserResponded;
-        uint256 maxTime; //unix time in seconds
+        uint256 expirationTime; //timestamp it should expire (unix in seconds)
         uint256 maxResponses;
-        uint256 rewardEth;
+        uint256 reward; //in wei
     }
 
     struct Account {
@@ -20,12 +21,12 @@ contract SurveyManager {
         uint256[] activeSurveys;
     }
 
-    uint256 public constant HOST_CUT = 1; //flat rate in eth
+    uint256 public constant HOST_CUT = 5000; //flat rate in wei
 
     mapping(address=>bool) isRegistered;
     mapping(string=>bool) isNameTaken;
     mapping(address=>Account) registeredUsers;
-    mapping(uint256=>Survey) surveyByID;
+    mapping(uint256=>Survey) surveyById;
     Survey[] public activeSurveys;
     uint256 public nextSurveyId;
 
@@ -36,7 +37,7 @@ contract SurveyManager {
 
     // Registers sender if not already registered
     // If the username or address is already registered, return false
-    function register(string username) public returns (bool) {
+    function register(string calldata username) public returns (bool) {
         if (isRegistered[msg.sender] || isNameTaken[username]) {
             return false;
         }
@@ -55,53 +56,105 @@ contract SurveyManager {
     // Make sure the user is registered
     // Make sure that the user has inputted enough tokens to cover the reward & other fees
     // Make a survey object and add it into the survey list.
-    function createSurvey(string question, string[] answers, uint expiration_time, uint response_cap, uint pooled_reward) public returns (bool) {
+    function createSurvey(string calldata question, string[] calldata answers, uint256 expiration_time, uint256 response_cap, uint256 pooled_reward) public returns (bool) {
         if (!isRegistered[msg.sender]) { // Make sure the user is registered
             return false;
         }
-        uint256 remainingETH = pooled_reward - host_cut;
+        uint256 remainingETH = pooled_reward - HOST_CUT; //todo: factor in gas costs?
+        if (uint256(remainingETH / response_cap) <= 0) { // not enough ETH to divide among user particiants
+            return false;
+        }
+        
+        Survey memory newSurvey = Survey({
+            owner: address(msg.sender),
+            id: nextSurveyId,
+            question: question,
+            answers: answers,
+            answerCounts: new uint256[](answers.length),
+            expirationTime: block.Timestamp + expiration_time,
+            maxResponses: response_cap,
+            reward: pooled_reward
+        });
+        surveyById[nextSurveyId] = newSurvey;
+        activeSurveys.push(newSurvey);
+        nextSurveyId++;
+
+        return true;
     }
     
     // Returns true if successfully responded to
     // Records answer and adds it to the responded mapping in the survey
-    function surveyRespond(int surveyId, int answer)  public returns (bool) {
+    function surveyRespond(int surveyId, int answer) external returns (bool) {
+        if(!surveyById[surveyId].active){
+            return false;
+        }
         if(surveyById[surveyId].hasUserResponded[msg.sender]){
             return false;
         }
-        surveyById[surveyId].answers[answer]++;
-        surveyById[surveyI]
+        surveyById[surveyId].answerCount[answer]++;
+        surveyById[surveyId].hasUserResponded[msg.sender] = true;
         return true;
     }
     
 
     // User has to be logged in, returns a string for each survey they own
-    function getOwnSurveys() public returns (uint256[]) {
-        if(isRegistered[msg.sender]){
-            return registeredUsers[msg.sender].activeSurveys;
-        }else{
-            return [];
-        }
+    function getOwnSurveys() external returns (uint256[] memory) {
+        require(isRegistered[msg.sender], "Must be registered to get surveys");
+        return registeredUsers[msg.sender].activeSurveys;
     }
     // Gets all surveys for a currently active
-    function getActiveSurveys() public returns (uint256[]) {}
-    // Gets all options for the given survey
-    function getAnswerOptions(uint256 surveyId)  public returns (string[]) {
-        if(!surveyById[surveyId].active){
-            return [];
-        }else{
-            return surveyById[surveyId].answers;
+    function getActiveSurveys() external returns (uint256[] memory) {
+        uint256[] memory activeSurveyIds;
+        for(int i = 0 ; i < activeSurveyIds.length; i++){        
+            activeSurveyIds.push(activeSurveys[i].id); 
         }
+        return activeSurveyIds; 
+    }
+
+    // Gets all options for the given survey
+    function getAnswerOptions(uint256 surveyId)  external returns (string[] memory) {
+        require(surveyById[surveyId].active, "surveyId is not valid");
+        return surveyById[surveyId].answers;
     }
     // Gets the survey question
-    function getSurveyQuestion(uint256 surveyId) public returns (string){
-        if(!surveyById[surveyId].active){
-            return "";
-        }else{
-            return surveyById[surveyId].question;
-        }
+    function getSurveyQuestion(uint256 surveyId) external returns (string memory){
+        require(surveyById[surveyId].active, "surveyId is not valid");
+        return surveyById[surveyId].question;
     }
     // Closes survey, only if survey belongs to the caller and is not already closed, returns if successfully closed
-    function closeSurvey(uint256 surveyId) public returns (bool) {}
+    function closeSurvey(uint256 surveyId) public payable returns (bool) {
+        Survey memory survey = surveyById[surveyId];
+        if (msg.sender != survey.owner) {
+            return false;
+        }
+        
+        uint activeSurveyPos = -1;
+        for(uint256 i = 0 ; i < activeSurveys.length; i++){ 
+            if(activeSurveys[i].id == surveyId){
+                activeSurveyPos = i;
+                break;
+            }
+        }
+        
+        if (activeSurveyPos < 0) { // check if survey is closed
+            return false;
+        }
+
+        //Remove Survey from active surveys
+        Survey memory temp;
+        temp = activeSurveys[activeSurveyPos];
+        activeSurveys[activeSurveyPos] = activeSurveys[activeSurveys.lenght - 1];
+        activeSurveys[activeSurveys.length - 1] = temp;
+        activeSurveys.pop();
+        
+        //Distribute eth
+
+        uint256 remainingEth = survey.response - HOST_CUT;
+        uint256 perPersonEth = remainingEth / survey.
+        
+
+        return true;
+    }
 }
 
 
